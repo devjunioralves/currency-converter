4b4f8e35563f341f26945f34a97b3d2be5c2127c
Junior Alves <jrwanderley17@gmail.com>
2023-03-31 18:12:54 -0300

    feat: ensure ConversionController return 500 if ConvertCurrency throws

--- src/presentation/controllers/conversion/Conversion.ts
+++ src/presentation/controllers/conversion/Conversion.ts
@@ -1,7 +1,7 @@
 import { type IConvertCurrency } from "@/domain/usecases/ConvertCurrency";
 import { InvalidParamError } from "@/presentation/errors/InvalidParamError";
 import { MissingParamError } from "@/presentation/errors/MissingParamError";
-import { badRequest } from "@/presentation/helpers/HttpHelper";
+import { badRequest, serverError } from "@/presentation/helpers/HttpHelper";
 import {  type IController } from "@/presentation/protocols/IController";
 import { type IHttpRequest, type IHttpResponse } from "@/presentation/protocols/IHttp";
 
@@ -13,33 +13,37 @@ export class ConversionController implements IController {
   }
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const requiredFields = ['from', 'to', 'value']
+    try {
+        const requiredFields = ['from', 'to', 'value']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+
+      const validCurrencies = ['USD', 'BRL', 'EUR', 'JPY']
+
+      if (!validCurrencies.includes(httpRequest.body.from)) {
+        return badRequest(new InvalidParamError('from'))
+      }
+
+      if (!validCurrencies.includes(httpRequest.body.to)) {
+        return badRequest(new InvalidParamError('to'))
+      }
+
+      await this.convertCurrency.convert({
+        from: httpRequest.body.from,
+        to: httpRequest.body.to,
+        amount: httpRequest.body.value
+      })
+
+      return await new Promise(resolve => { resolve({
+        statusCode: 400,
+        body: new Error('Missing param: coin')
+      }); })
+    } catch (error) {
+      return serverError(error as Error)
     }
-
-    const validCurrencies = ['USD', 'BRL', 'EUR', 'JPY']
-
-    if (!validCurrencies.includes(httpRequest.body.from)) {
-      return badRequest(new InvalidParamError('from'))
-    }
-
-    if (!validCurrencies.includes(httpRequest.body.to)) {
-      return badRequest(new InvalidParamError('to'))
-    }
-
-    await this.convertCurrency.convert({
-      from: httpRequest.body.from,
-      to: httpRequest.body.to,
-      amount: httpRequest.body.value
-    })
-
-    return await new Promise(resolve => { resolve({
-      statusCode: 400,
-      body: new Error('Missing param: coin')
-    }); })
   }
 }
