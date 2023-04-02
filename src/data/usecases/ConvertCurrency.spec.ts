b880c3a0f73055596e96c6776a8bf88b52572b16
Junior Alves <jrwanderley17@gmail.com>
2023-04-02 11:58:50 -0300

    test: ensure ConvertyCurrency use case call AddConversionRepository with correct values

--- src/data/usecases/ConvertCurrency.spec.ts
+++ src/data/usecases/ConvertCurrency.spec.ts
@@ -1,6 +1,8 @@
-import { type IConvertedCurrencyModel } from "@/domain/models/IConvertedCurrency"
-import { type IConvertCurrencyModel } from "@/domain/usecases/ConvertCurrency"
-import { ConvertCurrency } from "./ConvertCurrency"
+import { type IConvertedCurrencyModel } from '@/domain/models/IConvertedCurrency'
+import { type ITransaction } from '@/domain/models/ITransaction'
+import { type IConvertCurrencyModel } from '@/domain/usecases/ConvertCurrency'
+import { type IAddConversionRepository } from '../protocols/AddConversionRepository'
+import { ConvertCurrency } from './ConvertCurrency'
 
 interface IConverter {
   convert: (data: IConvertCurrencyModel) => Promise<IConvertedCurrencyModel>
@@ -9,28 +11,59 @@ interface IConverter {
 interface SutTypes {
   sut: ConvertCurrency
   converterStub: IConverter
+  addConversionRepositoryStub: IAddConversionRepository
 }
 
 const makeFakeConvertedCurrency = (): IConvertedCurrencyModel => ({
   date: '2018-02-22',
-  historical: "",
+  historical: '',
   info: {
     rate: 148.972231,
-    timestamp: 1519328414
+    timestamp: 1519328414,
   },
   query: {
     amount: 25,
-    from: "GBP",
-    to: "JPY"
+    from: 'GBP',
+    to: 'JPY',
   },
   result: 3724.305775,
-  success: true
+  success: true,
 })
+
+const makeAddConversionRepository = (): IAddConversionRepository => {
+  class AddConversionRepositoryStub implements IAddConversionRepository {
+    async add(data: IConvertedCurrencyModel): Promise<ITransaction> {
+      return await new Promise((resolve) => {
+        resolve({
+          id: 'any_id',
+          date: '2018-02-22',
+          historical: '',
+          info: {
+            rate: 148.972231,
+            timestamp: 1519328414,
+          },
+          query: {
+            amount: 25,
+            from: 'GBP',
+            to: 'JPY',
+          },
+          result: 3724.305775,
+          success: true,
+        })
+      })
+    }
+  }
+  return new AddConversionRepositoryStub()
+}
 
 const makeConverter = (): IConverter => {
   class ConverterStub implements IConverter {
-    async convert (data: IConvertCurrencyModel): Promise<IConvertedCurrencyModel> {
-      return await new Promise(resolve => { resolve(makeFakeConvertedCurrency()); })
+    async convert(
+      data: IConvertCurrencyModel
+    ): Promise<IConvertedCurrencyModel> {
+      return await new Promise((resolve) => {
+        resolve(makeFakeConvertedCurrency())
+      })
     }
   }
   return new ConverterStub()
@@ -38,10 +71,12 @@ const makeConverter = (): IConverter => {
 
 const makeSut = (): SutTypes => {
   const converterStub = makeConverter()
-  const sut = new ConvertCurrency(converterStub)
+  const addConversionRepositoryStub = makeAddConversionRepository()
+  const sut = new ConvertCurrency(converterStub, addConversionRepositoryStub)
   return {
     sut,
-    converterStub
+    converterStub,
+    addConversionRepositoryStub
   }
 }
 
@@ -52,23 +87,38 @@ describe('ConvertCurrency UseCase', () => {
     await sut.convert({
       from: 'USD',
       to: 'BRL',
-      amount: 1
+      amount: 1,
     })
     expect(convertSpy).toHaveBeenCalledWith({
       from: 'USD',
       to: 'BRL',
-      amount: 1
+      amount: 1,
     })
   })
 
   it('Should throw error if converter throws', async () => {
     const { sut, converterStub } = makeSut()
-    jest.spyOn(converterStub, 'convert').mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()); }))
+    jest.spyOn(converterStub, 'convert').mockReturnValueOnce(
+      new Promise((resolve, reject) => {
+        reject(new Error())
+      })
+    )
     const promise = sut.convert({
       from: 'USD',
       to: 'BRL',
-      amount: 1
+      amount: 1,
     })
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call AddConversionRepository with correct values', async () => {
+    const { sut, addConversionRepositoryStub } = makeSut()
+    const addSpy = jest.spyOn(addConversionRepositoryStub, 'add')
+    await sut.convert({
+      from: 'USD',
+      to: 'BRL',
+      amount: 1,
+    })
+    expect(addSpy).toHaveBeenCalledWith(makeFakeConvertedCurrency())
   })
 })
