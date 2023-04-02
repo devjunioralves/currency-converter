3bfcd1981610a5629428b1463091f5aed6490a11
Junior Alves <jrwanderley17@gmail.com>
2023-04-02 14:49:51 -0300

    refactor: change module to ConversionApi

--- src/infra/ConversionApi/ConversionApiAdapter.spec.ts
+++ src/infra/ConversionApi/ConversionApiAdapter.spec.ts
@@ -1,29 +1,31 @@
-import { ConversionApiAdapter } from "./ConversionApiAdapter"
+import { ConversionApiAdapter } from './ConversionApiAdapter'
 
 const makeSut = (): ConversionApiAdapter => {
   return new ConversionApiAdapter()
 }
 
-global.fetch = jest.fn(async () =>
-  await Promise.resolve({
-    json: async () => await Promise.resolve({
-      id: 'any_id',
-      date: '2018-02-22',
-      historical: '',
-      info: {
-        rate: 148.972231,
-        timestamp: 1519328414,
-      },
-      query: {
-        amount: 25,
-        from: 'GBP',
-        to: 'JPY',
-      },
-      result: 3724.305775,
-      success: true,
-    }),
-  }),
-) as jest.Mock;
+global.fetch = jest.fn(
+  async () =>
+    await Promise.resolve({
+      json: async () =>
+        await Promise.resolve({
+          id: 'any_id',
+          date: '2018-02-22',
+          historical: '',
+          info: {
+            rate: 148.972231,
+            timestamp: 1519328414,
+          },
+          query: {
+            amount: 25,
+            from: 'GBP',
+            to: 'JPY',
+          },
+          result: 3724.305775,
+          success: true,
+        }),
+    })
+) as jest.Mock
 
 describe('ConversionApiAdapter', () => {
   it('Should call ConversionApi', async () => {
@@ -66,7 +68,7 @@ describe('ConversionApiAdapter', () => {
     jest.spyOn(global, 'fetch').mockReturnValueOnce(
       new Promise((resolve, reject) => {
         reject(new Error())
-      }),
+      })
     )
     const promise = sut.convert({
       from: 'USD',
