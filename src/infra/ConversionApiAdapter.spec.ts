d31d434dd439b3f7b7265b35f0ebfee0171a3f65
Junior Alves <jrwanderley17@gmail.com>
2023-04-02 13:58:07 -0300

    feat: ensure ConversionApiAdapter return a conversion on success

--- src/infra/ConversionApiAdapter.spec.ts
+++ src/infra/ConversionApiAdapter.spec.ts
@@ -6,7 +6,22 @@ const makeSut = (): ConversionApiAdapter => {
 
 global.fetch = jest.fn(async () =>
   await Promise.resolve({
-    json: async () => await Promise.resolve({}),
+    json: async () => await Promise.resolve({
+      id: 'any_id',
+      date: '2018-02-22',
+      historical: '',
+      info: {
+        rate: 148.972231,
+        timestamp: 1519328414,
+      },
+      query: {
+        amount: 25,
+        from: 'GBP',
+        to: 'JPY',
+      },
+      result: 3724.305775,
+      success: true,
+    }),
   }),
 ) as jest.Mock;
 
@@ -19,5 +34,30 @@ describe('ConversionApiAdapter', () => {
       amount: 1,
     })
     expect(fetch).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should return a conversion on success', async () => {
+    const sut = makeSut()
+    const result = await sut.convert({
+      from: 'USD',
+      to: 'BRL',
+      amount: 1,
+    })
+    expect(result).toEqual({
+      id: 'any_id',
+      date: '2018-02-22',
+      historical: '',
+      info: {
+        rate: 148.972231,
+        timestamp: 1519328414,
+      },
+      query: {
+        amount: 25,
+        from: 'GBP',
+        to: 'JPY',
+      },
+      result: 3724.305775,
+      success: true,
+    })
   })
 })
